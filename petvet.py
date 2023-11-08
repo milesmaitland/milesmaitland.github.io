8bb9572d7f96ac7096217700ec922931ae7f4ff6
Miles Maitland Turner <146278568+milesmaitland@users.noreply.github.com>
2023-11-08 18:31:10 -0500

    Update petvet.py

--- petvet.py
+++ petvet.py
@@ -49,4 +49,45 @@ def get_dog_data():
     heart_yesno = input("\nWould you like to order monthly heartworm medication for " + pet_name + " (Y/N)? ")
     if heart_yesno.upper() == "Y":
         num_chews = int(input("\tHow many heart worm chews would you like to order?"))
+
+def perform_dog_calculations() :
+    global vax_cost , vax_name , chews_cost , total
+
+    if pet_vax_type == 1 :
+        vax_cost = PR_BORD
+        vax_name = "Bordatella"
+    elif pet_vax_type == 2
+        vax_cost = PR_DAPP
+        vax_name = "DAPP"
+    elif pet_vax_type == 3
+        vax_cost = PR_FLU
+        vax_name = "Influenza"
+    else: 
+        PR_ALL = PR_BORD + PR_DAPP + PR_FLU
+        vax_cost = .85 * PR_ALL
+        vax_name = "Full Vax Package"
+
+if num_chews ! =0:
+    if pet_weight < 25:
+        chews_cost = num_chews * PR_CHEWS_SMALL
+
+elif pet_weight >= 26 and pet_weight < 50:
+    chews_cost = num_chews * PR_CHEWS_LARGE
+
+total = vax_cost + chews_cost
+
+def display_dog_results() :
+    print ("DISPLAY DOGS")
+
+def get_cat_data():
+    print ("CAT CALCS")
+
+def perform_cat_calculations():
+    print ("CAT CALCS")
+
+def display_cat_results():
+    print("DISPLAY CATS")
+
+main()
+
     
